c36ba73e8664449413b3e87e91088f2f09fb6c64
Li <alice2005921@gmail.com>
2025-12-01 14:57:56 +0800

    v1_李羿慧

--- app/src/main/java/tw/edu/pu/csim/tcyang/s11312414/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/s11312414/MainActivity.kt
@@ -3,7 +3,6 @@ package tw.edu.pu.csim.tcyang.s11312414
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,13 +10,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.WindowInsetsCompat
+import android.view.Window
 import tw.edu.pu.csim.tcyang.s11312414.ui.theme.S11312414Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        val window: Window = window
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+
+        controller.hide(WindowInsetsCompat.Type.statusBars())
+        controller.hide(WindowInsetsCompat.Type.navigationBars())
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
+
+
         setContent {
+
             S11312414Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
@@ -31,17 +45,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
     Text(
+
         text = "Hello $name!",
         modifier = modifier
-    )
-}
 
+    )
+
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     S11312414Theme {
         Greeting("Android")
+
     }
 }
